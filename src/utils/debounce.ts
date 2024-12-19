83a3eb97983d3da1dcabe4cf795c193e56e0b0b3
DhirajsGithub <borsedhiraj123@gmail.com>
2024-12-20 00:16:24 +0530

    read me file + small fixes

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,10 +1,12 @@
-const debounce = (fn: Function, delay: number) => {
-    let timer;
-    return function (...args: any) {
+const debounce = (fn: (...args: any[]) => void, delay: number) => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    return function (...args: any[]) {
         clearTimeout(timer);
         timer = setTimeout(() => {
             fn(...args);
         }, delay);
-    }
-}
+    };
+};
+
 export { debounce };
